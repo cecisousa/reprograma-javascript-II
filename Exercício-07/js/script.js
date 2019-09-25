9aba2746e3baa1308ea57636e08a7120c5c1b894
Cecilia Sousa <ceciliasousa21@gmail.com>
2019-09-25 16:31:31 -0300

    cria e finaliza exercicio 7 sobre formulário

---
+++ Exercício-07/js/script.js
@@ -0,0 +1,28 @@
+const formulario = document.getElementById("formulario-de-comentarios")
+const todosOsComentarios = document.getElementById("todos-os-comentarios")
+const msgErro = document.querySelector(".erro")
+
+formulario.addEventListener("submit", function(evento){
+    evento.preventDefault()
+    let input = document.querySelector(".input-de-texto")
+    let mensagem = input.value
+    if (mensagem.trim() === ""){
+        // primeiro retorno possível: alert("O campo deve ser preenchido")
+        msgErro.textContent = "Preencha o campo!"
+        // o exemplo abaixo tem um bug, repetindo a mensagem de erro a cada clique
+        // const campoErro = document.querySelector("div")
+        // const msgErro = document.createElement("p")
+        // msgErro.textContent = "Preencha este campo!"
+        // campoErro.appendChild(msgErro)
+        // input.classList.add("erro-input")
+        // msgErro.classList.add("erro")
+    } else {
+        let novoComentario = document.createElement("p")
+        novoComentario.textContent = mensagem
+        todosOsComentarios.appendChild(novoComentario)
+        novoComentario.classList.add("comentario")
+        formulario.reset()
+        // outra forma de escrever a linha acima: input.value = ""
+        msgErro.textContent = ""
+    }
+})
